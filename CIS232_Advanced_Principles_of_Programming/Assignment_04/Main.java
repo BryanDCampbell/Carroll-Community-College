4af291ae06e97a4401c64de71597fe4b0827da65
Bryan Campbell <44508297+BryanDCampbell@users.noreply.github.com>
2020-07-06 11:44:33 -0400

    Update Main.java

--- CIS232_Advanced_Principles_of_Programming/Assignment_04/Main.java
+++ CIS232_Advanced_Principles_of_Programming/Assignment_04/Main.java
@@ -37,14 +37,108 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+private TextField myTextField;
+private Label capitalText;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage){
 
         //TODO: Write your Slot Machine GUI code here
+
+        //Set the stage title
+        primaryStage.setTitle("Slot Machine");
+
+        //AMOUNT INSERTED SECTION OF SLOT MACHINE
+        // Make a label control.
+        Label wagerLabel = new Label("Amount Inserted: $");
+        //Make a Text Field to enter text into.
+        myTextField = new TextField();
+        //Put the Label and Text Field in an HBox with 10 pixels of spacing and center alignment.
+        HBox wagerBox = new HBox(10, wagerLabel, myTextField);
+        wagerBox.setAlignment(Pos.CENTER);
+
+        //SPACETAKER
+        //Make a label control.
+        Label spaceTaker = new Label("                                   ");
+
+        //AMOUNT WON SECTION OF SLOT MACHINE
+        //Make a label control.
+        Label spinWins = new Label("Amount Won This Spin: $");  //NEED TO DO MORE
+        //Make a labe control.
+        Label totalWins = new Label("Total Amount Won: $");     //NEED TO DO MORE
+        //Put both Labels in a vbox with 10 pixels of spacing and center_right alignment.
+        VBox winsBox = new VBox(10, spinWins, totalWins);
+        winsBox.setAlignment(Pos.CENTER_RIGHT);
+
+        //COMBINATION OF AMOUNT INSERTED AND AMOUNT WON SECTIONS
+        HBox secondLayer = new HBox(10, wagerBox, spaceTaker, winsBox);
+
+
+        //Make a button to "Spin" the Slot Machine.
+        Button myButton = new Button("Spin");
+        //Register an event handler.
+        myButton.setOnAction(new ButtonClickHandler());
+        //Make a label that shows altered images (should be blank until button pushed).
+        capitalText = new Label();
+
+        //FRUIT WHEELS SECTION OF SLOT MACHINE
+        //image1
+        //image2
+        //image3
+        //HBox firstLayer = new HBox(10, image1, image2, image3);
+
+
+        //Put the HBox, Button, and Label in a VBox with 20 pixels of spacing and center alignment.
+        VBox vbox = new VBox(10, secondLayer, myButton, capitalText);
+        vbox.setAlignment(Pos.CENTER);
+
+        //Make the VBox the root node in the scene with width of 600 pixels and height of 200 pixels.
+        Scene scene = new Scene(vbox, 600, 200);
+        //Set the scene to the stage.
+        primaryStage.setScene(scene);
+        //Show the window.
+        primaryStage.show();
     }
 
+    class ButtonClickHandler implements EventHandler<ActionEvent> {
+        @Override
+        p public void handle(ActionEvent event) {
+            //Get the string to capitalize.
+            String input = myTextField.getText();
+            //Convert user input to double
+            double userAmountEntered = Double.parseDouble(input);
+
+            int imageIndex;
+            Random random = new Random();
+            imageIndex = random.nextInt(6);
+
+            if(imageIndex == 0){
+                //apple
+            } else if(imageIndex == 1){
+                //banana
+            } else if(imageIndex == 2){
+                //cherries
+            } else if(imageIndex == 3){
+                //grapes
+            } else if(imageIndex == 4){
+                //lemon
+            } else if(imageIndex == 5){
+                //lime
+            } else if(imageIndex == 6){
+                //orange
+            }else if(imageIndex == 7){
+                //pear
+            }else if(imageIndex == 8){
+                //strawberry
+            }else (imageIndex == 9){
+                //watermelon
+            }
+
+            //Display the results.
+            capitalText.setText(String.format(input.toUpperCase()));
+        }
 
     public static void main(String[] args) {
         launch(args);
